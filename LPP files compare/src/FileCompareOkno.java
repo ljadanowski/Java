3f4b79d2226395e0c8e9bdacd8053b705bdeab26
unknown <ljadanowski@LPP_LJADANOWSKI.headquarter.company.lpp.com.pl>
2014-12-31 14:36:06 +0100

    v1.0

--- LPP files compare/src/FileCompareOkno.java
+++ LPP files compare/src/FileCompareOkno.java
@@ -4,19 +4,47 @@ import java.awt.event.ActionListener;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
 
 
 public class FileCompareOkno extends JFrame implements ActionListener {
+
+	private static final long serialVersionUID = 1L;
 	JButton banalizuj;
 	JLabel pierwszyPlik, drugiPlik;
+	JMenuBar menuBar;
+	JMenu menuProgram, menuNarzedzia;
+	JMenuItem mKonwertuj, mOProgramie, mWyjscie;
 	
 	public FileCompareOkno() {
 		
 		setSize(400,300);
-		setTitle("LPP FILES' COMPARER");
+		setTitle("LPP FILES' COMPARER v1.0");
 		setLayout(null);
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setVisible(true);
+		
+		menuBar = new JMenuBar();
+		setJMenuBar(menuBar);
+		
+		menuProgram = new JMenu("Program");
+		menuBar.add(menuProgram);
+		menuNarzedzia = new JMenu("Narzedzia");
+		menuBar.add(menuNarzedzia);
+		
+		mKonwertuj = new JMenuItem("Konwertuj");
+		mOProgramie = new JMenuItem("O programie");
+		mWyjscie = new JMenuItem("Wyjscie");
+		
+		menuProgram.add(mOProgramie);
+		menuProgram.addSeparator();
+		menuProgram.add(mWyjscie);
+		menuNarzedzia.add(mKonwertuj);
+		
+		mWyjscie.addActionListener(this);
+		
 		banalizuj = new JButton("Analizuj!");
 		banalizuj.setBounds(120, 180, 120, 30);
 		add(banalizuj);
@@ -36,8 +64,8 @@ public class FileCompareOkno extends JFrame implements ActionListener {
 	}
 	@Override
 	public void actionPerformed(ActionEvent e) {
-		//System.out.println("Test...");
 		Object source = e.getSource();
+		if(source == mWyjscie) dispose();
 	}
 
 }
