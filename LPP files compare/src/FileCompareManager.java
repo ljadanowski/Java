5a218fd3924298569d43a343837b0437b120f3a6
unknown <ljadanowski@LPP_LJADANOWSKI.headquarter.company.lpp.com.pl>
2015-01-09 16:22:26 +0100

    Dodano nowe pliki np. pomoc

--- LPP files compare/src/FileCompareManager.java
+++ LPP files compare/src/FileCompareManager.java
@@ -1,16 +1,41 @@
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Scanner;
 
 
 public class FileCompareManager {
-
-	/**
-	 * @param args
-	 */
+	File plik;
+	Scanner scanner;
+	FileWriter zapis;
+	public void convert2csv(String sciezka, String separator) {
+	    plik = new File(sciezka);
+	    try {
+			zapis = new FileWriter(plik);
+		} catch (IOException e1) {
+			e1.printStackTrace();
+		}
+		try {
+		    scanner = new Scanner(plik);
+			while(scanner.hasNext()) {
+				;
+			}
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		}
+		scanner.close();	
+		try {
+			zapis.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
 	public static void main(String[] args) {
-		File directory = new File("D:\\raporty");
-		File[] contents = directory.listFiles();
-		for ( File f : contents) 
-		 System.out.println(f.getAbsolutePath());
+//		File directory = new File("D:\\raporty");
+//		File[] contents = directory.listFiles();
+//		for ( File f : contents) 
+//		 System.out.println(f.getAbsolutePath());
 		
 	}
 }
--- LPP files compare/src/FileCompareOkno.java
+++ LPP files compare/src/FileCompareOkno.java
@@ -1,29 +1,36 @@
+import java.awt.TextArea;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.File;
 
 import javax.swing.JButton;
+import javax.swing.JFileChooser;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JMenu;
 import javax.swing.JMenuBar;
 import javax.swing.JMenuItem;
+import javax.swing.JOptionPane;
+import javax.swing.JTextArea;
 
 
 public class FileCompareOkno extends JFrame implements ActionListener {
 
 	private static final long serialVersionUID = 1L;
-	JButton banalizuj;
+	JButton banalizuj, bPrzegladaj, bPrzegladaj2;
 	JLabel pierwszyPlik, drugiPlik;
 	JMenuBar menuBar;
 	JMenu menuProgram, menuNarzedzia;
 	JMenuItem mKonwertuj, mOProgramie, mWyjscie;
+	JTextArea notatnik;
 	
 	public FileCompareOkno() {
 		
-		setSize(400,300);
+		setSize(600,400);
 		setTitle("LPP FILES' COMPARER v1.0");
 		setLayout(null);
-		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		//setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
 		setVisible(true);
 		
 		menuBar = new JMenuBar();
@@ -44,11 +51,7 @@ public class FileCompareOkno extends JFrame implements ActionListener {
 		menuNarzedzia.add(mKonwertuj);
 		
 		mWyjscie.addActionListener(this);
-		
-		banalizuj = new JButton("Analizuj!");
-		banalizuj.setBounds(120, 180, 120, 30);
-		add(banalizuj);
-		banalizuj.addActionListener(this);
+		mOProgramie.addActionListener(this);
 		
 		pierwszyPlik = new JLabel("Pierwszy plik: ");
 		pierwszyPlik.setBounds(50, 50, 120, 30);
@@ -57,6 +60,26 @@ public class FileCompareOkno extends JFrame implements ActionListener {
 		drugiPlik = new JLabel("Drugi plik: ");
 		drugiPlik.setBounds(50, 100, 120, 30);
 		add(drugiPlik);
+		
+		bPrzegladaj = new JButton("Przegladaj dla 1 pliku...");
+		bPrzegladaj.setBounds(150, 50, 190, 20);
+		add(bPrzegladaj);
+		bPrzegladaj.addActionListener(this);
+		
+		bPrzegladaj2 = new JButton("Przegladaj dla 2 pliku...");
+		bPrzegladaj2.setBounds(150, 110, 190, 20);
+		add(bPrzegladaj2);
+		bPrzegladaj2.addActionListener(this);
+		
+		banalizuj = new JButton("Analizuj!");
+		banalizuj.setBounds(120, 180, 120, 30);
+		add(banalizuj);
+		banalizuj.addActionListener(this);
+		
+		notatnik = new JTextArea();
+		notatnik.setBounds(120, 200, 200, 80);
+		add(notatnik);
+		
 	}
 	public static void main(String[] args) {
 		FileCompareOkno fco = new FileCompareOkno();
@@ -66,6 +89,19 @@ public class FileCompareOkno extends JFrame implements ActionListener {
 	public void actionPerformed(ActionEvent e) {
 		Object source = e.getSource();
 		if(source == mWyjscie) dispose();
+		else if(source == mOProgramie) JOptionPane.showMessageDialog(this, "Program napisany przez Lukasza Jadanowskiego.");
+		else if(source == bPrzegladaj) {
+			JFileChooser fc = new JFileChooser();
+			if(fc.showOpenDialog(null) == JFileChooser.APPROVE_OPTION) {
+				File plik = fc.getSelectedFile();
+				String sciezka = plik.getAbsolutePath(); //getName()
+				System.out.println("Test: " + sciezka);
+			}
+		}
+		else if(source == bPrzegladaj2) {
+			File directory = new File("");
+			
+		}
 	}
 
 }
